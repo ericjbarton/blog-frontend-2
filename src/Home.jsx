d37c0963f838d0a746d51fb869e93b5706fef9c1
ericjbarton <erockbart@gmail.com>
2022-12-13 08:49:49 -0500

    adds a bunch of stuff

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,18 +6,33 @@ import { Modal } from "./Modal";
 
 export function Home() {
   const [posts, setPosts] = useState([]);
+  const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
+  const [currentPost, setCurrentPost] = useState({});
+
   const handleIndexPosts = () => {
     console.log("handleIndexPosts");
     axios.get("http://localhost:3000/posts.json").then((response) => {
       console.log(response.data);
       setPosts(response.data);
     });
-  }
+  };
+
+  const handleShowPost = (post) => {
+    console.log("handleShowPost", post);
+    setIsPostsShowVisible(true);
+    setCurrentPost(post);
+  };
+
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsPostsShowVisible(false);
+  };
+  
   useEffect(handleIndexPosts, []);
   
   return (
     <div>
-      < PostsIndex posts={posts}/>
+      < PostsIndex posts={posts} onShowPost={handleShowPost} />
       <Modal show={true} >
         <h1>Test</h1>
       </Modal>
