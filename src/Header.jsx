459c59facff4db5cf5e272aa42efa5222432709d
ericjbarton <erockbart@gmail.com>
2022-12-15 07:17:27 -0500

    adds styling, navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,19 @@
 export function Header() {
   return (
     <header>
-      <div><p> In javascript, "b" + "a" + +"a" + "a" = { "b" + "a" + +"a" + "a" } </p>
-      <nav>
-        <a href="#">Home</a> | <a href="#PostsIndex">All Posts</a>
-      </nav></div>
+      <div><nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Bloggy McBlogSpace</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button> <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto"> <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li> <li class="nav-item"> <a class="nav-link" href="#">Features</a>
+          </li>  <li class="nav-item">
+            <a class="nav-link" href="#">Yep!</a> </li> </ul> <span class="navbar-text">
+      The World's Greatest Gobbledygook </span>
+        </div>
+      </nav>
+      </div>
     </header>
-  )
+  );
 }
