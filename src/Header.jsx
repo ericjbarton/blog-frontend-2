488f3d62156380d6038955f32f05056d58bb2800
ericjbarton <erockbart@gmail.com>
2022-12-15 08:48:22 -0500

    adds className fixes. attempts at cards. WTF error city.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,22 @@
 export function Header() {
   return (
     <header>
-      <div><nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Bloggy McBlogSpace</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button> <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto"> <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li> <li class="nav-item"> <a class="nav-link" href="#">Features</a>
-          </li>  <li class="nav-item">
-            <a class="nav-link" href="#">Yep!</a> </li> </ul> <span class="navbar-text">
+      <div><nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">Bloggy McBlogSpace</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button> <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto"> <li className="nav-item active"> <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li> <li className="nav-item"> <a className="nav-link" href="#">Features</a>
+          </li>  <li className="nav-item">
+            <a className="nav-link" href="#">Yep!</a> </li> </ul> <span className="navbar-text">
       The World's Greatest Gobbledygook </span>
         </div>
       </nav>
       </div>
+      <br></br>
     </header>
   );
 }
+
+
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,16 +1,16 @@
 export function PostsIndex (props) {
-  return ( <div>
-    <h1>All Posts</h1>
-    {props.posts.map((post) => (
-      <div key={post.id}>
-        <h2>{post.title}</h2>
-        <a href="PostsShow"><img src={post.image}/></a>
-        <br></br>
-        <button onClick={() => props.onShowPost(post)}>More Info</button>
-      </div>
-    ))}
-  </div> 
+  console.log(props);
+  return ( 
+    <div id="posts-index">
+      <h1>All Posts</h1>
+      {props.posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <a href="PostsShow"><img src={post.image}/></a>
+          <br></br>
+          <button onClick={() => props.onShowPost(post)}>More Info</button>
+        </div>
+      ))} 
+    </div>
   );
 }
-
-
