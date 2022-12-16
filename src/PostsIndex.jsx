1a1bf39aad63402a7e0eb29f7792d8e659dc3ee7
ericjbarton <erockbart@gmail.com>
2022-12-16 11:16:45 -0500

    what's with the cards

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -2,7 +2,15 @@ export function PostsIndex (props) {
   console.log(props);
   return ( 
     <div id="posts-index">
+
+
       <h1>All Posts</h1>
+
+      <div className="card">
+        <div className="row">
+          <div className="card w-100">
+            <div className="card-body">
+  
       {props.posts.map((post) => (
         <div key={post.id}>
           <h2>{post.title}</h2>
@@ -11,6 +19,10 @@ export function PostsIndex (props) {
           <button onClick={() => props.onShowPost(post)}>More Info</button>
         </div>
       ))} 
+      </div>
+      </div>
     </div>
+    </div>
+      </div>
   );
 }
--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,8 +1,32 @@
+import axios from "axios"
+
 export function PostsShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.patch(`http://localhost:3000/posts/${props.post.id}.json`, params).then((response) => {
+      console.log(response.data);
+      event.target.reset();
+    });
+  };
+
   return (
-    <div>
+    <div id="posts-show">
       <h1>{props.post.title}</h1>
       <p>{props.post.body}</p>
+
+      <h1>Update Post</h1>
+      <form onSubmit={handleSubmit}><div>
+    Title: <input defaultValue={props.post.title} name="title" type="text" />
+      </div>
+      <div>
+    Gives us a verse, drop some knowledge: <input defaultValue={props.post.body} name="body" type="text" />
+      </div>
+      <div>
+    Add an Image URL <input defaultValue={props.post.image} name="image" type="text" />
+      </div>
+      <button type="submit">Update Post</button>
+      </form>
     </div>
-  );
+  );  
 }
